f90455e78bda84171fa5a15b39d54e70401c7bd2
David Maxwell <maxwelldavid512@gmail.com>
2022-04-25 20:00:37 -0300

    25/04

--- pages/faq.php
+++ pages/faq.php
@@ -1,51 +1,53 @@
-<h1>Perguntas Frequentes</h1>
+<div class="pag_faq">
+    <h1>Perguntas Frequentes</h1>
 
-<details>
-    <summary>O Nearby seleciona vagas apenas na área de programação?</summary>
-    <p>Sim, nossa plataforma só seleciona vagas voltadas para a área de programação. </p>
-</details>
-<details>
-    <summary>Preciso ter formação de nível superior para me candidatar a alguma vaga?</summary>
-    <p>Os pré-requisitos para as vagas dependem exclusivamente da empresa que está contratando. Entretanto, há vagas que não requer formação de nível superior.</p>
-</details>
-<details>
-    <summary>Quanto custa para utilizar a plataforma do Nearby?</summary>
-    <p>Não será necessário realizar nenhum pagamento, pois nossa plataforma é 100% gratuita.</p>
-</details>
-<details>
-    <summary>Não encontrei nenhuma vaga para o meu perfil, o que faço?</summary>
-    <p>Não precisa se preocupar. Ao realizar o cadastro no nosso site, seus dados ficam salvo no nosso banco de talentos, e assim que tivermos oportunidades de emprego que se encaixem no seu perfil, você será notificado na plataforma.</p>
-</details>
-<details>
-    <summary>Como saber a minha senioridade?</summary>
-    <p>Normalmente são as empresas que definem seu nível de senioridade, então se você já trabalhou na área, utilize a senioridade do seu último emprego, se não, você pode marcar como "Junior" e depois, caso necessário, alterá-la. </p>
-</details>
-<details>
-    <summary>Como me tornar um programador?</summary>
-    <p>Existem várias maneiras para se tornar um programador, uma delas é iniciar em um dos diversos cursos na área de tecnologia. Entretanto, não é obrigatório ter uma formação, então você pode optar por aprender de forma autodidata.</p>
-    <p>Fonte: <a href="https://www.digitalhouse.com/br/blog/faculdade-para-ser-programador/#:~:text=Para%20quem%20quer%20se%20tornar,Ci%C3%AAncia%20da%20Computa%C3%A7%C3%A3o%2C%20por%20exemplo">www.digitalhouse.com</a></p>
-</details>
-<details>
-    <summary>Como ingressar na área de T.I.?</summary>
-    <p>Se você já sabe a carreira que deseja seguir, o primeiro passo será escolher um curso voltado para essa área.</p> <p>Mas caso ainda não tenha decidido, não precisa se preocupar. Pesquise um pouco sobre as diversas possibilidades existente no ramo da T.I., e em seguida, basta escolher a que mais te agradar e/ou a que mais combinar com você. Se mesmo assim você não conseguir se decidir, escolha um curso que aborde mais de uma área, e ao logo dele você se identificará com alguma.</p>
-    <p>Após escolher o curso, você pode seguir os passos contidos nesse link: <a href="https://blog.ampli.com.br/guia-de-profissoes/carreira-em-ti/">8 passos para iniciar sua carreira de T.I.</a></p>
-</details>
-<details>
-    <summary>Vale a pena iniciar na programação depois dos 30?</summary>
-    <p>Quando observamos o mercado de tecnologia, vemos que a grande maioria da galera que está entrando nele são jovens, e isso pode fazer com que as pessoas mais velhas fiquem com receio de entrar nesse meio. Mas sim, vale a pena iniciar depois dos 30 ou de qualquer idade.</p>
-    <p>Geralmente as empresas quando vão contratar um analista, programador, ou alguem de qualquer outra área de T.I., buscam por pessoas profissionalmente qualificadas, e não necessariamente por jovens.</p>
-    <p>Fonte: <a href="https://uilblognews.com/programador-depois-dos-30/">Programador depois dos 30</a></p>
-</details>
-<details>
-    <summary>Curso livre ou faculdade, Qual escolher para entrar em T.I.? </summary>
-    <p>A resposta pra essa pergunta é um pouco pessoal, pois vai depender das suas necessidades, e dos seus objetivos. Cada um tem suas vantagens, e para saber mais sobre elas, basta acessar o link à seguir, e ver qual a melhor opção para o seu perfil. </p>
-    <a href="https://becode.com.br/carreira-ti-cursos-ou-faculdade/#:~:text=A%20faculdade%20%C3%A9%20a%20melhor,com%20pol%C3%ADticas%20r%C3%ADgidas%20de%20recrutamento">Curso livre ou Faculdade?</a>
-</details>
-<details>
-    <summary>Sou iniciante na programação, qual a primeira linguagem para eu aprender?</summary>
-    <p>Se você é iniciante, e não conhece nada de programação, a primeira coisa a aprender deveria ser lógica de programação. Com ela você terá uma base para entender as linguagens futuramente.</p>
-    <p>Após aprender lógica, se você ainda não souber qual área da programação quer seguir, sugiro aprender HTML e CSS, que são a base para uma página web. O HTML é uma linguagem utilizada para estruturar as páginas web, e o CSS é utilizado para dar o estilo dessa página. </p>
-    <p>Por que HTML e CSS?</p>
-    <p>São fáceis de aprender, não requer um hardware muito bom para começar a desenvolver, já que você precisará apenas de um bloco de notas e um navegador (mas é recomendável utilizar outros programas no lugar do bloco de notas), e dá pra criar muitas coisas apenas com essas duas tecnologias.</p>
-    <p>Fonte: <a href="https://www.digitalhouse.com/br/blog/linguagem-de-programacao-para-iniciantes/">Linguagem de programação para iniciantes</a></p>
-</details>
+    <details>
+        <summary>O Nearby seleciona vagas apenas na área de programação?</summary>
+        <p>Sim, nossa plataforma só seleciona vagas voltadas para a área de programação. </p>
+    </details>
+    <details>
+        <summary>Preciso ter formação de nível superior para me candidatar a alguma vaga?</summary>
+        <p>Os pré-requisitos para as vagas dependem exclusivamente da empresa que está contratando. Entretanto, há vagas que não requer formação de nível superior.</p>
+    </details>
+    <details>
+        <summary>Quanto custa para utilizar a plataforma do Nearby?</summary>
+        <p>Não será necessário realizar nenhum pagamento, pois nossa plataforma é 100% gratuita.</p>
+    </details>
+    <details>
+        <summary>Não encontrei nenhuma vaga para o meu perfil, o que faço?</summary>
+        <p>Não precisa se preocupar. Ao realizar o cadastro no nosso site, seus dados ficam salvo no nosso banco de talentos, e assim que tivermos oportunidades de emprego que se encaixem no seu perfil, você será notificado na plataforma.</p>
+    </details>
+    <details>
+        <summary>Como saber a minha senioridade?</summary>
+        <p>Normalmente são as empresas que definem seu nível de senioridade, então se você já trabalhou na área, utilize a senioridade do seu último emprego, se não, você pode marcar como "Junior" e depois, caso necessário, alterá-la. </p>
+    </details>
+    <details>
+        <summary>Como me tornar um programador?</summary>
+        <p>Existem várias maneiras para se tornar um programador, uma delas é iniciar em um dos diversos cursos na área de tecnologia. Entretanto, não é obrigatório ter uma formação, então você pode optar por aprender de forma autodidata.</p>
+        <p>Fonte: <a href="https://www.digitalhouse.com/br/blog/faculdade-para-ser-programador/#:~:text=Para%20quem%20quer%20se%20tornar,Ci%C3%AAncia%20da%20Computa%C3%A7%C3%A3o%2C%20por%20exemplo">www.digitalhouse.com</a></p>
+    </details>
+    <details>
+        <summary>Como ingressar na área de T.I.?</summary>
+        <p>Se você já sabe a carreira que deseja seguir, o primeiro passo será escolher um curso voltado para essa área.</p> <p>Mas caso ainda não tenha decidido, não precisa se preocupar. Pesquise um pouco sobre as diversas possibilidades existente no ramo da T.I., e em seguida, basta escolher a que mais te agradar e/ou a que mais combinar com você. Se mesmo assim você não conseguir se decidir, escolha um curso que aborde mais de uma área, e ao logo dele você se identificará com alguma.</p>
+        <p>Após escolher o curso, você pode seguir os passos contidos nesse link: <a href="https://blog.ampli.com.br/guia-de-profissoes/carreira-em-ti/">8 passos para iniciar sua carreira de T.I.</a></p>
+    </details>
+    <details>
+        <summary>Vale a pena iniciar na programação depois dos 30?</summary>
+        <p>Quando observamos o mercado de tecnologia, vemos que a grande maioria da galera que está entrando nele são jovens, e isso pode fazer com que as pessoas mais velhas fiquem com receio de entrar nesse meio. Mas sim, vale a pena iniciar depois dos 30 ou de qualquer idade.</p>
+        <p>Geralmente as empresas quando vão contratar um analista, programador, ou alguem de qualquer outra área de T.I., buscam por pessoas profissionalmente qualificadas, e não necessariamente por jovens.</p>
+        <p>Fonte: <a href="https://uilblognews.com/programador-depois-dos-30/">Programador depois dos 30</a></p>
+    </details>
+    <details>
+        <summary>Curso livre ou faculdade, Qual escolher para entrar em T.I.? </summary>
+        <p>A resposta pra essa pergunta é um pouco pessoal, pois vai depender das suas necessidades, e dos seus objetivos. Cada um tem suas vantagens, e para saber mais sobre elas, basta acessar o link à seguir, e ver qual a melhor opção para o seu perfil. </p>
+        <a href="https://becode.com.br/carreira-ti-cursos-ou-faculdade/#:~:text=A%20faculdade%20%C3%A9%20a%20melhor,com%20pol%C3%ADticas%20r%C3%ADgidas%20de%20recrutamento">Curso livre ou Faculdade?</a>
+    </details>
+    <details>
+        <summary>Sou iniciante na programação, qual a primeira linguagem para eu aprender?</summary>
+        <p>Se você é iniciante, e não conhece nada de programação, a primeira coisa a aprender deveria ser lógica de programação. Com ela você terá uma base para entender as linguagens futuramente.</p>
+        <p>Após aprender lógica, se você ainda não souber qual área da programação quer seguir, sugiro aprender HTML e CSS, que são a base para uma página web. O HTML é uma linguagem utilizada para estruturar as páginas web, e o CSS é utilizado para dar o estilo dessa página. </p>
+        <p>Por que HTML e CSS?</p>
+        <p>São fáceis de aprender, não requer um hardware muito bom para começar a desenvolver, já que você precisará apenas de um bloco de notas e um navegador (mas é recomendável utilizar outros programas no lugar do bloco de notas), e dá pra criar muitas coisas apenas com essas duas tecnologias.</p>
+        <p>Fonte: <a href="https://www.digitalhouse.com/br/blog/linguagem-de-programacao-para-iniciantes/">Linguagem de programação para iniciantes</a></p>
+    </details>
+</div>
